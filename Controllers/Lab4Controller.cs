fe8c30ed4884c0afc83a3559a95649f12bba456b
davinom <36043102+davinom@users.noreply.github.com>
2018-02-15 17:36:01 -0500

    Add files via upload

---
+++ Controllers/Lab4Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        // GET: Lab4
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Page2(int id)
+        {
+            return View(id);
+        }
+
+
+        public ActionResult Page3()
+        {
+            ViewBag.Title = "Beverages";
+            string[] drinks = { "Coffee", "Tea", "Beer", "Coke", "Wine" };
+            ViewData["drinks"]=drinks;
+            return View(drinks);
+        }
+    }
+}
